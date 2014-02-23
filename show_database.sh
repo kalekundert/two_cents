277c844fbf5242e16679232b4b2dde1fbb98e766
Kale Kundert <kale@thekunderts.net>
2014-02-23 09:08:10 -0800

    Improve the scripts that work with the database.

---
+++ show_database.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+sqliteman ~/.config/budget/budget.db &
--- wipe_database.sh
+++ wipe_database.sh
@@ -2,7 +2,7 @@
 
 rm -f ~/.config/budget/budget.db
 
-./main.py add-bank wells-fargo
-./main.py add-account grocery
-./main.py add-account restaurant
-./main.py add-account miscellaneous
+#./main.py add-bank wells-fargo
+#./main.py add-account grocery
+#./main.py add-account restaurant
+#./main.py add-account miscellaneous
