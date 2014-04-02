034f9f56d1fd3b4cdda2a489a6775809bc6840e2
Kale Kundert <kale@thekunderts.net>
2014-04-01 21:37:46 -0700

    Add a script to install dependencies on fedora.

---
+++ yum.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+sudo yum install            \
+    python3                 \
+    python3-ofxparse        \
+    python3-requests        \
+    python3-sqlalchemy      \
+    python3-PyYAML          \
+
